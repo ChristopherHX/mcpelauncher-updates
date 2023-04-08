977e8d3de66119ba0ca8921126b873bb21c0d4f9
ChristopherHX <christopher.homberger@web.de>
2023-04-08 17:15:39 +0200

    ?

--- src/main.cpp
+++ src/main.cpp
@@ -178,8 +178,8 @@ template<class ...jnitypes> struct InterfaceFactory {
 						MDispatch<std::tuple_element_t<I, composeType>, jobject>::CallMethod ...,
 						MDispatch<std::tuple_element_t<I, composeType>, jobject, jclass>::CallMethod ...,
 						NULL,
-						(void*)(NullTemplate<IM1>::value)...,
-						(void*)(NullTemplate<IM1>::value)...,
+						(NullTemplate<IM1>::value ? nullptr : nullptr)...,
+						(NullTemplate<IM1>::value ? nullptr : nullptr)...,
 						NULL,
 						MDispatch<std::tuple_element_t<I, composeType>, jclass>::CallMethod ...,
 					};
