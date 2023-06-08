c43d6bc810ece3fd775dd1d4804729aa16a73803
ChristopherHX <christopher.homberger@web.de>
2023-06-08 19:41:18 +0200

    finialize malloc(+64)

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <dlfcn.h>
 #include <malloc.h>
 
 extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
@@ -5,6 +6,7 @@ extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
     if(!h) {
         return;
     }
+    void (*mcpelauncher_preinithook)(const char*sym, void*val, void **orig);
     mcpelauncher_preinithook = (decltype(mcpelauncher_preinithook)) dlsym(h, "mcpelauncher_preinithook");
     mcpelauncher_preinithook("malloc", (void*)+[](size_t s) {
 	return malloc(s + 64);
